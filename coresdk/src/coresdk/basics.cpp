e4ae0eb0f389457030125b1405b25b4926b15562
Kiandisor <mojynardi@gmail.com>
2026-07-21 14:24:16 +1000

    Refactored basics source file
    Removed the using statements in the basics source file

--- coresdk/src/coresdk/basics.cpp
+++ coresdk/src/coresdk/basics.cpp
@@ -20,23 +20,23 @@
 namespace splashkit_lib
 {
 
-    const string BASE64_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    const std::string BASE64_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
     // trim see: https://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
 
     // trim from start
-    string ltrim(const string &text)
+    std::string ltrim(const std::string &text)
     {
-        string s = text;
+        std::string s = text;
         s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int c)
                                         { return !std::isspace(c); }));
         return s;
     }
 
     // trim from end
-    string rtrim(const string &text)
+    std::string rtrim(const std::string &text)
     {
-        string s = text;
+        std::string s = text;
         s.erase(std::find_if(s.rbegin(), s.rend(), [](int c)
                              { return !std::isspace(c); })
                     .base(),
@@ -45,51 +45,51 @@ namespace splashkit_lib
     }
 
     // trim from both ends
-    string trim(const string &text)
+    std::string trim(const std::string &text)
     {
         return ltrim(rtrim(text));
     }
 
-    string to_lowercase(const string &text)
+    std::string to_lowercase(const std::string &text)
     {
-        string data = text;
+        std::string data = text;
         std::transform(data.begin(), data.end(), data.begin(), ::tolower);
         return data;
     }
 
-    string to_uppercase(const string &text)
+    std::string to_uppercase(const std::string &text)
     {
-        string data = text;
+        std::string data = text;
         std::transform(data.begin(), data.end(), data.begin(), ::toupper);
         return data;
     }
 
-    int length_of(const string &text)
+    int length_of(const std::string &text)
     {
         return text.length();
     }
 
-    bool contains(const string &text, const string &subtext)
+    bool contains(const std::string &text, const std::string &subtext)
     {
         return index_of(text, subtext) != -1;
     }
 
-    int index_of(const string &text, const string &subtext)
+    int index_of(const std::string &text, const std::string &subtext)
     {
         size_t pos = text.find(subtext);
-        if (pos == string::npos)
+        if (pos == std::string::npos)
             return -1;
         return static_cast<int>(pos);
     }
 
-    string replace_all(const string &text, const string &substr, const string &new_text)
+    std::string replace_all(const std::string &text, const std::string &substr, const std::string &new_text)
     {
         if (substr.empty())
             return text;
 
-        string result = text;
+        std::string result = text;
         size_t pos = 0;
-        while ((pos = result.find(substr, pos)) != string::npos)
+        while ((pos = result.find(substr, pos)) != std::string::npos)
         {
             result.replace(pos, substr.length(), new_text);
             pos += new_text.length();
@@ -97,12 +97,12 @@ namespace splashkit_lib
         return result;
     }
 
-    vector<string> split(const string &text, char delimiter)
+    std::vector<string> split(const std::string &text, char delimiter)
     {
-        vector<string> result;
-        string::size_type start = 0;
-        string::size_type end = text.find(delimiter);
-        while (end != string::npos)
+        std::vector<string> result;
+        std::string::size_type start = 0;
+        std::string::size_type end = text.find(delimiter);
+        while (end != std::string::npos)
         {
             result.push_back(text.substr(start, end - start));
             start = end + 1;
@@ -114,9 +114,9 @@ namespace splashkit_lib
 
     // integer check see: https://stackoverflow.com/questions/2844817/how-do-i-check-if-a-c-string-is-an-int#2845275
 
-    bool is_integer(const string &text)
+    bool is_integer(const std::string &text)
     {
-        string s = trim(text);
+        std::string s = trim(text);
         if (s.empty() || ((!isdigit(s[0])) && (s[0] != '-') && (s[0] != '+')))
             return false;
 
@@ -126,14 +126,14 @@ namespace splashkit_lib
         return (*p == 0);
     }
 
-    bool is_double(const string &text)
+    bool is_double(const std::string &text)
     {
         return is_number(text);
     }
 
-    bool is_number(const string &text)
+    bool is_number(const std::string &text)
     {
-        string s = trim(text);
+        std::string s = trim(text);
         if (s.empty() || ((!isdigit(s[0])) && (s[0] != '-') && (s[0] != '+')))
             return false;
 
@@ -143,17 +143,17 @@ namespace splashkit_lib
         return (*p == 0);
     }
 
-    int convert_to_integer(const string &text)
+    int convert_to_integer(const std::string &text)
     {
         return std::stoi(text);
     }
 
-    double convert_to_double(const string &text)
+    double convert_to_double(const std::string &text)
     {
         return std::stod(text);
     }
 
-    bool is_binary(const string &bin_str)
+    bool is_binary(const std::string &bin_str)
     {
         for (char c : bin_str)
         {
@@ -163,7 +163,7 @@ namespace splashkit_lib
         return !bin_str.empty();
     }
 
-    bool is_hex(const string &hex_str)
+    bool is_hex(const std::string &hex_str)
     {
         for (char c : hex_str)
         {
@@ -173,7 +173,7 @@ namespace splashkit_lib
         return !hex_str.empty();
     }
 
-    bool is_octal(const string &octal_str)
+    bool is_octal(const std::string &octal_str)
     {
         for (char c : octal_str)
         {
@@ -183,13 +183,13 @@ namespace splashkit_lib
         return !octal_str.empty();
     }
 
-    string dec_to_bin(unsigned int a_dec)
+    std::string dec_to_bin(unsigned int a_dec)
     {
         // Without this check, dec to bin will not work if dec is 0
         if (a_dec == 0)
             return "0";
 
-        string bin_string;
+        std::string bin_string;
         while (a_dec > 0)
         {
             bin_string = ((a_dec & 1) ? "1" : "0") + bin_string;
@@ -198,26 +198,26 @@ namespace splashkit_lib
         return bin_string;
     }
 
-    unsigned int bin_to_dec(const string &bin_str)
+    unsigned int bin_to_dec(const std::string &bin_str)
     {
         if (!is_binary(bin_str))
         {
-            LOG(ERROR) << "Invalid binary string \"" << bin_str << "\" passed to bin_to_dec. Returning 0.";
+            LOG(ERROR) << "Invalid binary std::string \"" << bin_str << "\" passed to bin_to_dec. Returning 0.";
             return 0;
         }
 
         return stoi(bin_str, nullptr, 2);
     }
 
-    string hex_to_bin(const string &hex_str)
+    std::string hex_to_bin(const std::string &hex_str)
     {
         if (!is_hex(hex_str))
         {
-            LOG(ERROR) << "Invalid hexadecimal string \"" << hex_str << "\" passed to hex_to_bin. Returning empty string.";
+            LOG(ERROR) << "Invalid hexadecimal std::string \"" << hex_str << "\" passed to hex_to_bin. Returning empty std::string.";
             return "";
         }
 
-        string bin_string;
+        std::string bin_string;
         for (char hex_char : hex_str)
         {
             int hex_val = 0;
@@ -238,25 +238,25 @@ namespace splashkit_lib
         if (hex_str.length() == 1)
         {
             size_t first_one = bin_string.find_first_not_of('0');
-            return (first_one == string::npos) ? "0" : bin_string.substr(first_one);
+            return (first_one == std::string::npos) ? "0" : bin_string.substr(first_one);
         }
 
         return bin_string;
     }
 
-    string bin_to_hex(const string &bin_str)
+    std::string bin_to_hex(const std::string &bin_str)
     {
         if (!is_binary(bin_str))
         {
-            LOG(ERROR) << "Invalid binary string \"" << bin_str << "\" passed to bin_to_hex. Returning empty string.";
+            LOG(ERROR) << "Invalid binary std::string \"" << bin_str << "\" passed to bin_to_hex. Returning empty std::string.";
             return "";
         }
 
-        string hex_string;
+        std::string hex_string;
         int length = bin_str.length();
 
         int padding = (4 - (length % 4)) % 4;
-        string padded_bin_str = string(padding, '0') + bin_str;
+        std::string padded_bin_str = std::string(padding, '0') + bin_str;
 
         for (size_t i = 0; i < padded_bin_str.length(); i += 4)
         {
@@ -276,14 +276,14 @@ namespace splashkit_lib
         return hex_string;
     }
 
-    string dec_to_oct(unsigned int decimal_value)
+    std::string dec_to_oct(unsigned int decimal_value)
     {
         if (decimal_value == 0)
         {
             return "0";
         }
 
-        string octal_string;
+        std::string octal_string;
         while (decimal_value > 0)
         {
             octal_string = std::to_string(decimal_value % 8) + octal_string;
@@ -292,37 +292,37 @@ namespace splashkit_lib
         return octal_string;
     }
 
-    unsigned int oct_to_dec(const string &octal_string)
+    unsigned int oct_to_dec(const std::string &octal_string)
     {
         if (!is_octal(octal_string))
         {
-            LOG(ERROR) << "Invalid octal string \"" << octal_string << "\" passed to oct_to_dec. Returning 0.";
+            LOG(ERROR) << "Invalid octal std::string \"" << octal_string << "\" passed to oct_to_dec. Returning 0.";
             return 0;
         }
 
         return stoi(octal_string, nullptr, 8);
     }
 
-    unsigned int hex_to_dec(const string &hex_string)
+    unsigned int hex_to_dec(const std::string &hex_string)
     {
         if (!is_hex(hex_string))
         {
-            LOG(ERROR) << "Invalid octal string \"" << hex_string << "\" passed to hex_to_dec. Returning 0.";
+            LOG(ERROR) << "Invalid octal std::string \"" << hex_string << "\" passed to hex_to_dec. Returning 0.";
             return 0;
         }
 
         return stoi(hex_string, nullptr, 16);
     }
 
-    string oct_to_bin(const string &octal_str)
+    std::string oct_to_bin(const std::string &octal_str)
     {
         if (!is_octal(octal_str))
         {
-            LOG(ERROR) << "Invalid octal string \"" << octal_str << "\" passed to oct_to_bin. Returning empty string.";
+            LOG(ERROR) << "Invalid octal std::string \"" << octal_str << "\" passed to oct_to_bin. Returning empty std::string.";
             return "";
         }
 
-        string bin_string;
+        std::string bin_string;
         for (char oct_char : octal_str)
         {
             int oct_val = oct_char - '0';
@@ -335,22 +335,22 @@ namespace splashkit_lib
         }
 
         size_t first_one = bin_string.find_first_not_of('0');
-        return (first_one == string::npos) ? "0" : bin_string.substr(first_one);
+        return (first_one == std::string::npos) ? "0" : bin_string.substr(first_one);
     }
 
-    string bin_to_oct(const string &bin_str)
+    std::string bin_to_oct(const std::string &bin_str)
     {
         if (!is_binary(bin_str))
         {
-            LOG(ERROR) << "Invalid binary string \"" << bin_str << "\" passed to bin_to_oct. Returning empty string.";
+            LOG(ERROR) << "Invalid binary std::string \"" << bin_str << "\" passed to bin_to_oct. Returning empty std::string.";
             return "";
         }
 
-        string octal_string;
+        std::string octal_string;
 
-        // Pad binary string with leading zeros to make its length a multiple of 3
+        // Pad binary std::string with leading zeros to make its length a multiple of 3
         int padding = (3 - (bin_str.length() % 3)) % 3;
-        string padded_bin_str = string(padding, '0') + bin_str;
+        std::string padded_bin_str = std::string(padding, '0') + bin_str;
 
         for (size_t i = 0; i < padded_bin_str.length(); i += 3)
         {
@@ -366,36 +366,36 @@ namespace splashkit_lib
         }
 
         size_t first_non_zero = octal_string.find_first_not_of('0');
-        return (first_non_zero == string::npos) ? "0" : octal_string.substr(first_non_zero);
+        return (first_non_zero == std::string::npos) ? "0" : octal_string.substr(first_non_zero);
     }
 
-    string hex_to_oct(const string &hex_str)
+    std::string hex_to_oct(const std::string &hex_str)
     {
         if (!is_hex(hex_str))
         {
-            LOG(ERROR) << "Invalid hexadecimal string \"" << hex_str << "\" passed to hex_to_oct. Returning empty string.";
+            LOG(ERROR) << "Invalid hexadecimal std::string \"" << hex_str << "\" passed to hex_to_oct. Returning empty std::string.";
             return "";
         }
 
-        string bin_str = hex_to_bin(hex_str);
+        std::string bin_str = hex_to_bin(hex_str);
         return bin_to_oct(bin_str);
     }
 
-    string oct_to_hex(const string &octal_str)
+    std::string oct_to_hex(const std::string &octal_str)
     {
         if (!is_octal(octal_str))
         {
-            LOG(ERROR) << "Invalid octal string \"" << octal_str << "\" passed to oct_to_hex. Returning empty string.";
+            LOG(ERROR) << "Invalid octal std::string \"" << octal_str << "\" passed to oct_to_hex. Returning empty std::string.";
             return "";
         }
 
-        string bin_str = oct_to_bin(octal_str);
+        std::string bin_str = oct_to_bin(octal_str);
         return bin_to_hex(bin_str);
     }
 
-    string base64_encode(const string &input)
+    std::string base64_encode(const std::string &input)
     {
-        string encoded;
+        std::string encoded;
         int val = 0, bits = -6;
         const unsigned int b63 = 0x3F;
 
@@ -424,13 +424,13 @@ namespace splashkit_lib
         return encoded;
     }
 
-    string base64_decode(const string &input)
+    std::string base64_decode(const std::string &input)
     {
-        string decoded;
+        std::string decoded;
         int val = 0, bits = -8;
         for (unsigned char c : input)
         {
-            if (BASE64_CHARS.find(c) == string::npos)
+            if (BASE64_CHARS.find(c) == std::string::npos)
             {
                 if (c == '=')
                     break; // Padding character, stop decoding
